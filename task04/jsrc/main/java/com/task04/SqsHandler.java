726dbe0130ad3751e467d2e7b8a67df6c0f68b50
Andrii Tyzun <andrii.tyzun@gmail.com>
2024-12-01 23:36:54 +0200

    Initial commit

---
+++ task04/jsrc/main/java/com/task04/SqsHandler.java
@@ -0,0 +1,37 @@
+package com.task04;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import com.syndicate.deployment.annotations.events.SqsTriggerEventSource;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.resources.DependsOn;
+import com.syndicate.deployment.model.ResourceType;
+import com.syndicate.deployment.model.RetentionSetting;
+
+@LambdaHandler(
+    lambdaName = "sqs_handler",
+	roleName = "sqs_handler-role",
+	isPublishVersion = true,
+	aliasName = "${lambdas_alias_name}",
+	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+@SqsTriggerEventSource(
+		targetQueue = "async_queue",
+		batchSize = 10
+)
+@DependsOn(
+		name = "async_queue",
+		resourceType = ResourceType.SQS_QUEUE
+)
+public class SqsHandler implements RequestHandler<SQSEvent, Void> {
+
+	@Override
+	public Void handleRequest(SQSEvent event, Context context) {
+		event.getRecords().forEach(record -> {
+			String messageBody = record.getBody();
+			context.getLogger().log("Received SQS message: " + messageBody);
+		});
+		return null;
+	}
+}
