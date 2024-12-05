ee8d5c6c269021c49f407616b456bdfd13bbca36
Andrii Tyzun <andrii.tyzun@gmail.com>
2024-12-05 18:38:40 +0200

    Task 07 third commit

--- task07/jsrc/main/java/com/task07/UuidGenerator.java
+++ task07/jsrc/main/java/com/task07/UuidGenerator.java
@@ -16,26 +16,26 @@ import com.syndicate.deployment.model.RetentionSetting;
 import java.time.Instant;
 import java.util.*;
 
-@LambdaHandler(
-    lambdaName = "uuid_generator",
-	roleName = "uuid_generator-role",
-	isPublishVersion = true,
-	aliasName = "${lambdas_alias_name}",
-	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
-)
-@EventSource(eventType = EventSourceType.CLOUDWATCH_RULE_TRIGGER)
-@DependsOn(name = "uuid_trigger", resourceType = ResourceType.CLOUDWATCH_RULE)
-@EnvironmentVariable(key = "bucket", value = "${target_bucket}")
+//@LambdaHandler(
+//    lambdaName = "uuid_generator",
+//	roleName = "uuid_generator-role",
+//	isPublishVersion = true,
+//	aliasName = "${lambdas_alias_name}",
+//	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+//)
+//@EventSource(eventType = EventSourceType.CLOUDWATCH_RULE_TRIGGER)
+//@DependsOn(name = "uuid_trigger", resourceType = ResourceType.CLOUDWATCH_RULE)
+//@EnvironmentVariable(key = "bucket", value = "${target_bucket}")
 public class UuidGenerator implements RequestHandler<Object, Void> {
 
 	private final AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
 			.withRegion("eu-central-1")
 			.build();
-	private static final String BUCKET_NAME = System.getenv("BUCKET_NAME");
 
 	@Override
 	public Void handleRequest(Object input, Context context) {
 		try {
+			context.getLogger().log("UUID generation, bucket name: " + System.getenv("bucket"));
 			List<String> uuids = new ArrayList<>();
 			for (int i = 0; i < 10; i++) {
 				uuids.add(UUID.randomUUID().toString());
@@ -48,7 +48,7 @@ public class UuidGenerator implements RequestHandler<Object, Void> {
 
 			String fileName = Instant.now().toString();
 
-			s3Client.putObject(BUCKET_NAME, fileName, jsonContent);
+			s3Client.putObject(System.getenv("bucket"), fileName, jsonContent);
 			context.getLogger().log("File created: " + fileName);
 
 			return null;
